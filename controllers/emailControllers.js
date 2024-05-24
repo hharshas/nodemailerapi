303e73910e3ea22d1b5ea63520bd2689ee96e078
harsh singh <hs180002@gmail.com>
2024-05-24 13:39:03 +0530

    changes to test

--- controllers/emailControllers.js
+++ controllers/emailControllers.js
@@ -19,7 +19,7 @@ const sendEmail = expressAsyncHandler(async (req, res) => {
 
   var mailOptions = {
     from: process.env.SMTP_MAIL,
-    to: "hello@headshot.energy",
+    to: "hs1800002@gmail.com",
     subject: subject,
     text: message,
   };
