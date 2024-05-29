b36b824e6d95bbedcdca0519685006469916b6d9
harsh singh <hs180002@gmail.com>
2024-05-29 23:02:37 +0530

    chagnes

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const { sendEmail } = require("../controllers/emailControllers");
 
 router.post("/sendEmail", sendEmail);
+router.post("/keepalive", (req, res) => {
+    res.status(200).send("Server is alive");
+});
 
 module.exports = router;
